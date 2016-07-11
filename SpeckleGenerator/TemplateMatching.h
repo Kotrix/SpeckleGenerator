83570b4edf8800d4f63cf049a4eddcc1dcd705cd
Christopher <kotrix11@gmail.com>
2016-07-12 00:00:14 +0100

    Speckle Generator backup

---
+++ SpeckleGenerator/TemplateMatching.h
@@ -0,0 +1,71 @@
+﻿#pragma once
+#include "Method.h"
+#include "PeakFinderFactory.h"
+#include <iostream>
+
+class TemplateMatching : public Method
+{
+protected:
+	Ptr<PeakFinder> mPeakFinder;
+
+	
+	vector<Mat> generatePyramid(const Mat& img, const Rect& ROI) const
+	{
+		vector<Mat> result;
+		result.push_back(img(ROI));
+		Mat lower;
+		for (int i = 1; i <= mLayers; i++)
+		{
+			double f = pow(2, -i);
+			resize(img(ROI), lower, Size(), f, f);
+			result.push_back(lower);
+		}
+		return result;
+	}
+
+public:
+	explicit TemplateMatching(const Mat& first) : Method("TemplateMatching"), mTempRatio(0.5), mMaxShift(0.15), mLayers(0), mSegmentsVertically(1), mSegmentsHorizontally(1)
+	{
+		mPeakFinder = PeakFinderFactory::getPeakFinder(SPATIAL, SAD);
+
+		int width = first.cols;
+		int height = first.rows;
+
+		//calculate ROI for template
+		double borderRatio = (1.0 - mTempRatio) * 0.5;
+		Point tl = Point(floor(width * borderRatio), floor(height * borderRatio));
+		Point br = Point(ceil(width * (1.0 - borderRatio)), ceil(height * (1.0 - borderRatio)));
+		mTemplateROI = Rect(tl, br);
+		cout << "Template area set to: " << mTemplateROI << endl;
+
+		//calculate ROI for search area
+		borderRatio = (1.0 - mTempRatio - 2 * mMaxShift) * 0.5;
+		tl = Point(floor(width * borderRatio), floor(height * borderRatio));
+		br = Point(ceil(width * (1.0 - borderRatio)), ceil(height * (1.0 - borderRatio)));
+		mSearchROI = Rect(tl, br);
+		cout << "Search area set to: " << mSearchROI << endl;
+
+		mTemplates = generatePyramid(first, mTemplateROI);
+	}
+
+	Point3f getDisplacement(const Mat& img) override
+	{
+		vector<Mat> pyramid = generatePyramid(img, mSearchROI);
+
+		Point bestLoc = mPeakFinder->findPeak(pyramid[mLayers], mTemplates[mLayers]);
+
+		for (int i = mLayers - 1; i >= 0; i--)
+		{
+			bestLoc *= 2;
+			Point border = Point(1, 1);
+			if (bestLoc == Point(0, 0)) border = Point(0, 0);
+
+			bestLoc += mPeakFinder->findPeak(match(pyramid[i](Rect(bestLoc - border, mTemplates[i].size() + Size(2, 2))), mTemplates[i])) - border;
+		}
+		bestLoc += mSearchROI.tl() - mTemplateROI.tl();
+
+		mTemplates = generatePyramid(img, mTemplateROI);
+
+		return Point3f(bestLoc);
+	}
+};
---
+++ SpeckleGenerator/main.cpp
@@ -0,0 +1,134 @@
+/**
+Subpixel shifter
+@author Krzysztof Kotowski
+*/
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <iostream>
+#include <fstream>
+
+using namespace cv;
+using namespace std;
+
+double inline sign(double x)
+{
+	if (x < 0) return -1.0;
+	if (x > 0) return 1.0;
+	return 0.0;
+}
+
+Point2f pabs(Point2f point)
+{
+	return Point2f(fabs(point.x), fabs(point.y));
+}
+
+String getName(int number)
+{
+	String name = "images\\img";
+	for (int dec = 10; dec <= pow(10, 3); dec *= 10)
+	{
+		if (number < dec) name += "0";
+	}
+	name += to_string(number);
+	name += ".png";
+	return name;
+}
+
+int main()
+{
+	cout << "Speckle generator\n";
+
+	//base image for generator
+	Mat image = imread("C:\\Users\\Krzysztof\\Pictures\\data\\img01.png", IMREAD_GRAYSCALE);
+	//output for ground truth file
+	ofstream movesFile("images\\moves.txt");
+
+	Size dstSize = Size(640, 640); //size of images to generate
+	int moves = 100; //number of images (moves) to generate
+	Point2f direction = Point2f(5.0, -5.0); //starting direction
+	Point2f maxDev = Point2f(1.0, 1.0); //maximum deviation from direction
+	double precision = 0.1;
+	bool drawPath = false;
+
+	//check if enough space left for movement
+	Size emptySpace = image.size() - dstSize;
+	Point2f maxShift = pabs(direction) + pabs(maxDev);
+	if (emptySpace.width < maxShift.x || emptySpace.height < maxShift.y)
+	{
+		cout << "Too small source image for this translation" << endl;
+		return EXIT_FAILURE;
+	}
+
+	//set precision
+	if (precision > 1.0 || precision < 0.0)
+	{
+		cout << "Precision has to be positive and less than 1" << endl;
+		return EXIT_FAILURE;
+	}
+	int scale = ceil(pow(precision, -1)); //calculate minimum up-scale needed to achieve precision
+	precision = pow(scale, -1);
+	movesFile << scale << endl;
+
+	//up-scale to achieve given precision
+	Mat bigger;
+	resize(image, bigger, Size(), scale, scale, INTER_AREA);
+	Rect mBiggerROI = Rect((bigger.size() - dstSize * scale) / 2, dstSize * scale); //ROI in the centre
+	Point intDirection = Point(direction * scale);
+	Point intMaxDev = Point(pabs(maxDev) * scale);
+
+	RNG rng(0xFFFFFF);
+	Point position = Point(0, 0);
+
+	Mat dst; //matrix for generated image
+	//first image generate
+	resize(bigger(mBiggerROI), dst, dstSize, 0, 0, INTER_AREA);
+	imwrite(getName(0), dst);
+
+	if(drawPath) namedWindow("path", WINDOW_NORMAL);
+
+	for (int i = 1; i <= moves; i++)
+	{
+		//get next translation vector
+		Point move(intDirection.x + rng.uniform(-intMaxDev.x, intMaxDev.x), intDirection.y + rng.uniform(-intMaxDev.y, intMaxDev.y));
+		move.y = -move.y; //invert Y axis for Euclidean move
+		Point nextROIpoint = mBiggerROI.tl() + move;
+		
+		//change direction if boundary approached
+		if (nextROIpoint.x < 0 || nextROIpoint.x > bigger.cols - mBiggerROI.width)
+		{
+			intDirection.x = -intDirection.x;
+			move.x = -move.x;
+		}
+		if (nextROIpoint.y < 0 || nextROIpoint.y > bigger.rows - mBiggerROI.height)
+		{
+			intDirection.y = -intDirection.y;
+			move.y = -move.y;
+		}
+		
+		mBiggerROI += move;
+		move.y = -move.y; //re-invert Y axis for Euclidean move
+		position += move;
+
+		if (drawPath)
+		{
+			//draw and show path
+			Mat pathImg;
+			cvtColor(bigger, pathImg, CV_GRAY2BGR);
+			//line(pathImg, mBiggerROI.tl() - move, mBiggerROI.tl(), Scalar(0, 255, 0), 2 * scale);
+			rectangle(pathImg, mBiggerROI, Scalar(0, 0, 255), 2 * scale);
+			imshow("path", pathImg);
+			waitKey(1);
+		}
+
+		movesFile << position.x / (double)scale << "," << position.y / (double)scale << endl;
+		cout << i << ". " << position.x / (double)scale << "," << position.y / (double)scale << endl;
+
+		resize(bigger(mBiggerROI), dst, dstSize, 0, 0, INTER_AREA);
+		imwrite(getName(i), dst);
+	}
+
+	movesFile.close();
+	return EXIT_SUCCESS;
+}
+
