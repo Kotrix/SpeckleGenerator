9216ac6e9315ffd2cf6fa132ec3a705206bb9f3a
Christopher <kotrix11@gmail.com>
2016-08-19 15:27:47 +0100

    Final MSc version

--- SpeckleGenerator/main.cpp
+++ SpeckleGenerator/main.cpp
@@ -1,5 +1,6 @@
 /**
-Subpixel shifter
+Generator of sub-pixel displacements
+WARNING: HIGH RAM MEMORY USAGE FOR LARGE PRECISION!
 @author Krzysztof Kotowski
 */
 #include <opencv2/core.hpp>
@@ -11,45 +12,54 @@ Subpixel shifter
 using namespace cv;
 using namespace std;
 
-double inline sign(double x)
-{
-	if (x < 0) return -1.0;
-	if (x > 0) return 1.0;
-	return 0.0;
-}
+double inline sign(double x);
+Point2f pabs(Point2f point);
+Point2f addNoise(Mat& dst, double stdDev);
+String getName(int number, const String& folder);
 
-Point2f pabs(Point2f point)
+int main(int argc, char** argv)
 {
-	return Point2f(fabs(point.x), fabs(point.y));
-}
+	String folder = "images";
+	String source = "img01.png";
 
-String getName(int number)
-{
-	String name = "images\\img";
-	for (int dec = 10; dec <= pow(10, 3); dec *= 10)
-	{
-		if (number < dec) name += "0";
-	}
-	name += to_string(number);
-	name += ".png";
-	return name;
-}
-
-int main()
-{
 	cout << "Speckle movement generator\n";
-
-	//base image for generator
-	Mat image = imread("C:\\Users\\Krzysztof\\Pictures\\data\\img01.png", IMREAD_GRAYSCALE);
-	//output for ground truth file
-	ofstream movesFile("images\\moves.txt");
 
 	Size dstSize = Size(512, 512); //size of images to generate
 	int moves = 100; //number of images (moves) to generate
-	Point2f direction = Point2f(0, -13); //starting direction
-	Point2f maxDev = Point2f(0,0); //maximum deviation from direction
+	Point2f direction = Point2f(10, 10); //starting direction
+	Point2f maxDev = Point2f(2, 2); //maximum deviation from direction
+	double angle = 0;
+	double angleVar = 0;
+	double noise = 0;
 	double precision = 0.1;
-	bool drawPath = false;
+
+	/**
+	* USER INTERFACE
+	*
+	*/
+	if (argc > 2) //arguments from command window
+	{
+		if (argc > 1)  source = argv[1];
+		if (argc > 2)  folder = argv[2];
+		if (argc > 3)  dstSize = Size(stoi(argv[3]), stoi(argv[3]));
+		if (argc > 4)  moves = stoi(argv[4]);
+		if (argc > 5)  direction = Point2f(stod(argv[5]), stod(argv[5]));
+		if (argc > 6)  maxDev = Point2f(stod(argv[6]), stod(argv[6]));
+		if (argc > 7)  angle = stod(argv[7]);
+		if (argc > 8)  noise = stod(argv[8]);
+		if (argc > 9)  precision = stod(argv[9]);
+	}
+	else
+	{
+		cout << "Usage : %s <image> <dst_folder> <dst_size> <moves> <direction> <deviation> <angle> <noise> <precision> <\n", argv[0];
+		cout << "Using default parameters only\n";
+	}
+
+
+	//base image for generator
+	Mat image = imread(source, IMREAD_GRAYSCALE);
+	//output for ground truth file
+	ofstream movesFile(folder + "\\moves.txt");
 
 	//check if enough space left for movement
 	Size emptySpace = image.size() - dstSize;
@@ -72,17 +82,18 @@ int main()
 
 	//up-scale to achieve given precision
 	Mat bigger;
-	resize(image, bigger, Size(), scale, scale, INTER_AREA);
+	resize(image, bigger, Size(), scale, scale, INTER_NEAREST);
 	Rect mBiggerROI = Rect((bigger.size() - dstSize * scale) / 2, dstSize * scale); //ROI in the centre
 	Point intDirection = Point(direction * scale);
 	Point intMaxDev = Point(pabs(maxDev) * scale);
 
 	RNG rng(0xFFFFFF);
 	Point position = Point(0, 0);
+	double rotation = 0;
 
 	Mat dst;
-	if(drawPath) namedWindow("path", WINDOW_NORMAL);
-
+	Mat rotatedBigger = bigger.clone();
+	Point2f sumMeanDev(0);
 	for (int i = 0; i <= moves; i++)
 	{
 		if (i != 0)
@@ -90,6 +101,10 @@ int main()
 			//get next translation vector
 			Point move(intDirection.x + rng.uniform(-intMaxDev.x, intMaxDev.x), intDirection.y + rng.uniform(-intMaxDev.y, intMaxDev.y));
 			move.y = -move.y; //invert Y axis for Euclidean move
+
+			/*
+			TRANSLATION
+			*/
 			Point nextROIpoint = mBiggerROI.tl() + move;
 
 			//change direction if boundary approached
@@ -108,26 +123,107 @@ int main()
 			move.y = -move.y; //re-invert Y axis for Euclidean move
 			position += move;
 
-			if (drawPath)
-			{
-				//draw and show path
-				Mat pathImg;
-				cvtColor(bigger, pathImg, CV_GRAY2BGR);
-				//line(pathImg, mBiggerROI.tl() - move, mBiggerROI.tl(), Scalar(0, 255, 0), 2 * scale);
-				rectangle(pathImg, mBiggerROI, Scalar(0, 0, 255), 2 * scale);
-				imshow("path", pathImg);
-				waitKey(1);
-			}
 
-			movesFile << position.x * precision << "," << position.y * precision << endl;
-			cout << i << ". " << position.x * precision << "," << position.y * precision << endl;
+			/*
+			ROTATION
+			*/
+			if (abs(angle) > FLT_EPSILON)
+			{
+				Point2f pc(bigger.cols / 2.0 + position.x, bigger.rows / 2.0 - position.y);
+				rotation += angle + rng.uniform(-angleVar, angleVar);
+				Mat r = getRotationMatrix2D(pc, rotation, 1.0);
+				warpAffine(bigger, rotatedBigger, r, bigger.size());
+			}
+			else
+				bigger.copyTo(rotatedBigger);
+
+			/*
+			OUTPUT TO FILE AND SCREEN
+			*/
+			movesFile << position.x * precision << "," << position.y * precision << "," << rotation << endl;
+			cout << i << ". " << position.x * precision << "," << position.y * precision << "," << rotation << endl;
 		}
 
-		resize(bigger(mBiggerROI), dst, dstSize, 0, 0, INTER_AREA);
-		imwrite(getName(i), dst);
+		/*
+		SHRINKING
+		*/
+		resize(rotatedBigger(mBiggerROI), dst, dstSize, 0., 0., INTER_AREA);
+
+		/*
+		NOISE
+		*/
+		if (noise > DBL_EPSILON)
+		{
+			sumMeanDev += addNoise(dst, noise);
+		}
+
+		imwrite(getName(i, folder), dst);
+	}
+	movesFile.close();
+	bigger.release();
+	rotatedBigger.release();
+	dst.release();
+
+	return EXIT_SUCCESS;
+}
+
+
+double inline sign(double x)
+{
+	if (x < 0) return -1.0;
+	if (x > 0) return 1.0;
+	return 0.0;
+}
+
+Point2f pabs(Point2f point)
+{
+	return Point2f(fabs(point.x), fabs(point.y));
+}
+
+Point2f addNoise(Mat& dst, double stdDev)
+{
+	Mat noise(dst.size(), CV_8SC1);
+
+	//generate normal distributed noise using default random generator
+	theRNG().state = getTickCount();
+	randn(noise, 0, stdDev);
+
+	Scalar mean, dev;
+	meanStdDev(noise, mean, dev);
+
+	//add noise to input without overflows - the fastest way
+	Mat output = dst.clone();
+	uchar *pInput, *pOutput;
+	char *pNoise;
+	for (int i = 0; i < dst.rows; i++)
+	{
+		pInput = dst.ptr<uchar>(i);
+		pOutput = output.ptr<uchar>(i);
+		pNoise = noise.ptr<char>(i);
+		for (int j = 0; j < dst.cols; j++)
+		{
+			char change = pNoise[j];
+			if (change != 0)
+			{
+				int out = pInput[j] + (int)change;
+				pOutput[j] = uchar(out > 255 ? 255 : out < 0 ? 0 : out); //clamp to (0,255)
+			}
+		}
 	}
 
-	movesFile.close();
-	return EXIT_SUCCESS;
+	output.copyTo(dst);
+	return Point2f(mean[0], dev[0]);
+}
+
+String getName(int number, const String& folder)
+{
+	String name = folder + "\\img";
+	for (int dec = 10; dec <= pow(10, 3); dec *= 10)
+	{
+		if (number < dec) name += "0";
+	}
+	name += to_string(number);
+	name += ".png";
+	return name;
 }
 
